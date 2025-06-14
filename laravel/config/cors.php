cce19a0557330b1f8b64a7442c323ab76450df3d
Mengleang30 <novmengleang14@gmail.com>
2025-06-14 16:12:32 +0700

    fix pay1

--- laravel/config/cors.php
+++ laravel/config/cors.php
@@ -15,7 +15,7 @@ return [
     |
     */
 
-    'paths' => ['api/*', 'sanctum/csrf-cookie','login/google/callback'],
+    'paths' => ['api/*', 'sanctum/csrf-cookie','login/google/callback','capture-payment'],
 
     'allowed_methods' => ['*'],
 
