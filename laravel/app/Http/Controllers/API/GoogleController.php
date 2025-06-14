dfd06b53edd043a1bb038bf27982e9160dac9351
Mengleang30 <novmengleang14@gmail.com>
2025-06-14 13:48:27 +0700

    fix / problem3

--- laravel/app/Http/Controllers/API/GoogleController.php
+++ laravel/app/Http/Controllers/API/GoogleController.php
@@ -54,7 +54,7 @@ class GoogleController
           // Generate API token for the user
           $token = $user->createToken('GoogleLogin')->plainTextToken;
 
-          return redirect()->to("https://e-commerce-book-store.up.railway.app/auth/google/callback?token=$token");
+          return redirect()->to("https://e-commerce-book-store.up.railway.app?token=$token");
 
           return response()->json(['token' => $token]);
 
