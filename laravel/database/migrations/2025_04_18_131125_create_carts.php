b351d244666e23f10728e264abfd563c90ae7ec7
Mengleang30 <novmengleang14@gmail.com>
2025-06-14 15:11:24 +0700

    fix direct problem7

--- laravel/database/migrations/2025_04_18_131125_create_carts.php
+++ laravel/database/migrations/2025_04_18_131125_create_carts.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('carts', function (Blueprint $table) {
             $table->id();
             $table->bigInteger('user_id')->unsigned();
+            $table->decimal('grand_total');
             $table->timestamps();
         });
     }
